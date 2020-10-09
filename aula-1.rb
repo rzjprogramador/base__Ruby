9c4cece668bb84d79586a090ef210c7b6356ebdb
rzjprogramador <rzjprogramador@gmail.com>
2020-10-08 23:05:19 -0300

    Classes Ruby

---
+++ aula-1.rb
@@ -0,0 +1,15 @@
+h = {}
+puts 1.class
+puts 1.class.superclass
+puts 1.class.superclass.superclass
+puts 4.3.class
+puts 4.3.class.superclass
+puts nil.class
+puts h.class
+puts :symbol.class
+puts [].class
+puts (1..8).class
+
+a = 1
+
+puts a.class
