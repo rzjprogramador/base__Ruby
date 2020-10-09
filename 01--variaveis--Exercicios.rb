2bcb8953c8225320b974592122abe6a4a70855db
rzjprogramador <rzjprogramador@gmail.com>
2020-10-09 01:01:57 -0300

    EXERCICIO VARIAVEIS DE INSTANCIA -- EXEMPLO GENERICO

--- 01--variaveis--Exercicios.rb
+++ 01--variaveis--Exercicios.rb
@@ -4,14 +4,23 @@ Nome = "Reinaldo"
 
 class Mostranome
 
-	def nome
+	def metodoCriado
 		Nome
 	end
 end
 
-puts Mostranome.new.nome	
+puts Mostranome.new.metodoCriado	
 
+# OBS: O NOME DO METODO TEM QUE SER IGUAL O NOME DO NEW CLASSE QUE VAI SER INSTANCIADO
+# COMO ESTA NO EXEMPLO a função metodo e dps chamada fora da classe com mesmo nome
 
+VarDeclarada = "Ruby"
 
+class ClasseparaInstanciar
 
+	def funcaoparaInstanciar
+		VarDeclarada
+	end
+end
 
+puts ClasseparaInstanciar.new.funcaoparaInstanciar	
