0e5607ac4c125106fb67acf6e8d0fa8dbbd5c21c
rzjprogramador <rzjprogramador@gmail.com>
2020-10-08 23:36:53 -0300

    CHAMANDO A FUNÇÃO COM VARIAVEL SIMPLES FORA DA CLASSE -- OBS: VAI DAR ERRO

--- aula-1.rb
+++ aula-1.rb
@@ -1,25 +1,13 @@
 require 'byebug'
 
-# VARIAVEL SIMPLES
+# FUNÇÃO PARA SER CHAMADA FORA DA CLASSE
+
 a = 1
-puts a
+def teste
+	puts a
+end
+
+# CHAMANDO A FUNÇÃO FORA DA CLASSE >
+teste
 
 
-# VARIAVEL DE INSTÂNCIA -- 
-@a = 2
-puts @a
-
-
-# VARIAVEL DE CLASSE -- Trafega entre classes -- as classes que herdeiras tem acesso a essa variavel .
-@@a = 3
-puts @@a
-
-
-# VARIAVEL GLOBAL -- CUIDADO ..Elas sobrecarregam o servidor
-$a = 4
-puts $a
-
-
-# VARIAVEL CONSTANTE -- ELAS PODEM SER USADAS EM OUTROS ESCOPOS -- NA MAIORIA DAS VEZES SÃO CLASSES --COMEÇAM COM MAIUSCULA
-A = 5
-puts A
