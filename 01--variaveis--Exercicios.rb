4028de6eb8ad672a1e8a14a7f1aa7969cb722dee
rzjprogramador <rzjprogramador@gmail.com>
2020-10-09 00:35:01 -0300

    MOSTRANDO PELO METODO nome -- O VALOR DA VARIAVEL CONSTANTE POR INSTANCIA FORA DA CLASSE

---
+++ 01--variaveis--Exercicios.rb
@@ -0,0 +1,17 @@
+# EXERCICIO MOSTRANDO PELO METODO nome -- O VALOR DA VARIAVEL CONSTANTE POR INSTANCIA FORA DA CLASSE
+
+Nome = "Reinaldo"
+
+class Mostranome
+
+	def nome
+		Nome
+	end
+end
+
+puts Mostranome.new.nome	
+
+
+
+
+
---
+++ 01--variaveis.rb
@@ -0,0 +1,23 @@
+require 'byebug'
+
+# VARIAVEL DECLARADA CONSTANTE PARA SER USADA DENTRO DA CLASSE
+
+
+A = 5
+
+class Teste
+
+	def a
+		A
+	end
+end	
+
+# CHAMANDO >>>
+
+puts Teste.new.a
+
+
+
+
+
+
