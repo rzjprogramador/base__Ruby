36bf9170c8b8f61d9027a41d3ba52101df582038
rzjprogramador <rzjprogramador@gmail.com>
2020-10-08 23:12:37 -0300

    Debugando codigo Ruby

--- aula-1.rb
+++ aula-1.rb
@@ -1,3 +1,6 @@
+require 'byebug'
+debugger
+
 h = {}
 puts 1.class
 puts 1.class.superclass
@@ -10,6 +13,7 @@ puts :symbol.class
 puts [].class
 puts (1..8).class
 
-a = 1
-
-puts a.class
+# Codigos byebug	
+# n	next --vai pro proximo passo
+# s	entra dentro do metodo
+# c	pra ir direto e acabar programa
