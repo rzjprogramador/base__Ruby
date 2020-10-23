b9ff3754ee143736f74ea1f642de1de1b4c0fd22
rzjprogramador <rzjprogramador@gmail.com>
2020-10-23 13:24:18 -0300

    Classe sem Acessors

--- 11--OO_aula--1.rb
+++ 11--OO_aula--1.rb
@@ -1,22 +1,84 @@
-# ORIENTAÇÃO A OBJETOS =  OBJETIVO :: ORGANIZAR OS CODIGOS EM CLASSES E METODOS CRIANDO UM MODELO QUE REPLICA SUAS PROPRIEDADES PARA SEREM ACESSADOS EM NOVOS CLONES
+require 'byebug'
+
+class Carro
+
+    def initialize(nome = "Padron-Default")
+        @nome = nome
+    end
+
+    def nome=(value)
+        @nome = value
+    end
+
+    def nome
+        @nome               
+    end
+
+    def mostrar(marca ="Marca Padrao OPCIONAL DEFAULT SÓ APARECE SE NA INVOCAÇÃO NAO FOR PREENCHIDO")    
+        puts "Marca: #{marca} -- Modelo: #{self.nome}"   
+    end
+
+end
+
+# debugger
+
+#INSTANCIAR
+carro = Carro.new
+
+# RECUPERAR
+
+carro.nome="Palio"
+carro.mostrar("Fiat")
+carro.mostrar()
+carro.mostrar("Fiat")
+
+
+
+=begin 
+
+# ORIENTAÇÃO A OBJETOS =  OBJETIVO :: 
+#ORGANIZAR OS CODIGOS EM CLASSES E METODOS CRIANDO UM MODELO QUE REPLICA SUAS PROPRIEDADES PARA SEREM ACESSADOS EM NOVOS CLONES
 
 # --CLASS  --INSTANCIA --CONSTRUTOR
     
-#  PARA DDDDD REAPROVEITAR
-#  CLASSE ::
-#  PEGAR TODOS CODIGOS , METODOS,FUNCOES E COLOCAR EM UMA ESTRUTURA DENIMINADA CLASSE
+# Classe é um modelo onde descrevemos as propriedades e Métodos do nosso Objeto (neste caso Objeto Carro)
+# Quando criamos uma Estancia :: Passamos essas propriedades e metodos para externos.
+# Criar Estancia com new e guardar numa variavel e começar a recuperar seus atributos e metodos
 
-# CRIANDO UMA CLASSE COM  OS METODOS ACESSORES  get & set ::
+# (get) GETTERS :: Atributo  SOMENTE LEITURA :: pode recuperar/acessar e somente ver o atributo (get) NÃO PERMITE adicionar valor.
+# (set) SETTERS :: Atributo que PERMITE ESCRITA ::PERMITE INSERIR  valor a este atributo (set) Pego por PARAMETRO ou não -- Mas ele sozinho não permite só leitura PRECISA DO GET pra pegar o atributo pra ele.
 
-# (get) GETTERS para PEGAR O VALOR de um atributo (get) 
-# (set) SETTERS :: para ALTERAR o valor deste atributo (set)
-
-=begin 
- Partindo do princípio que deseja saber sobre uma propriedade ou um método de prefixo set e get, 
-este padrão é usado para abstrair um estado que dever ser interno ao objeto. 
 =end
 
-require 'byebug'
+=begin 
+
+=========== MONTAGEM CLASSE :: =========================
+
+class Nome_classe
+    # Construtor do ruby é o initialize
+
+    def initialize(var = "Default Modelo padrao")
+        @variavel = variavel
+    end
+
+    def GET: nomeAtributo_GET
+        @variavel_de_instancia
+    end 
+    def SET : nomeAtributo_SET (value_OPCIONAL_PARA RECUPERARporPARAM_o_VALOR)
+    end
+
+    def metodo
+        puts fazer algo
+    end
+end >> # fecha a classe   
+
+=========================================================
+
+=end
+
+
+=begin
+# =======TODO CODIGO COMENTADO ::==========================
 
 class Carro
     # definido o getter
@@ -40,10 +102,14 @@ end
 
 carro = Carro.new       # Instanciando a classe para poder ser usada fora 
 
-# INVOCANDO CHAMANDO A CLASSE AGORA INSTANCIADA PARA APROVEITAR SEUS ATRIBUTOS , PROPRIEDADES E METODOS::
+# INVOCANDO/RECUPERANDO/CHAMANDO A CLASSE AGORA INSTANCIADA PARA APROVEITAR SEUS ATRIBUTOS , PROPRIEDADES E METODOS::
 
 carro.nome="Palio"      # Com a classe instanciada posso usar seus atributos e dos atributos as suas propriedades
 # puts carro.nome       # Agora vou usar carro.mostrar ja que temos um metodo
 carro.mostrar("Fiat")   # Mostrando da classe instanciada carro , o metodo mostrar com o parametro ...preenchido como Fiat    
 carro.mostrar()         # Nao preenchendo o parametro nesta invocacao --ele mostra o que definimos por default no parametro da funcao
 carro.mostrar("Fiat")   # Mesmo com o default no parametro ele sobreescreve e mostra o preenchido no parametro aqui
+
+=====================================================
+
+=end
